271dd2bca98a45270a4ef42360e1e1f441786795
Mohammed Abbasi <raamizabbasi@Mohammeds-MacBook-Pro.local>
2020-08-18 18:04:09 -0400

    GLITCH COMMIT: Added more code for Memo.view.js

--- client/views/Memo.view.js
+++ client/views/Memo.view.js
@@ -1,45 +1,96 @@
-import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native'
 import AddAHabitHeader from '../components/AddAHabitHeader'
 import YellowNoteHeader from '../components/YellowNoteHeader'
 import BlueInfoBox from '../components/BlueInfoBox'
 
 const Memo = () => {
+	const [text, changeText] = useState('')
+
+	const wordsRemaining = () => {
+		return 30 - text.split(' ').length + 1
+	}
+
 	return (
 		<>
 			<AddAHabitHeader />
-			<YellowNoteHeader text="Wow 3 times a day is a lot..." />
-            <View style={styles.body}>
-                <BlueInfoBox />
-                <View style={styles.questionContainer}>
-                    <Text style={styles.questionText}>Why Do You Want To Stop?</Text>
-                </View>
-                <View style={styles.textBoxContainer}>
-
-                </View>
-            </View>
+			<YellowNoteHeader text='Wow 3 times a day is a lot...' />
+			<View style={styles.body}>
+				<BlueInfoBox />
+				<View style={styles.questionContainer}>
+					<Text style={styles.questionText}>Why Do You Want To Stop?</Text>
+				</View>
+				<View style={styles.textBoxContainer}>
+					<TextInput
+						style={styles.textBox}
+						value={text}
+						onChangeText={(text) => changeText(text)}
+					/>
+					<Text
+						style={{
+							fontSize: 15,
+							marginTop: 20,
+							color: wordsRemaining() <= 5 ? 'red' : 'black',
+						}}
+					>
+						{wordsRemaining() || 30} words remaining
+					</Text>
+				</View>
+				<View style={styles.buttonContainer}>
+					<View style={styles.button}>
+						<TouchableOpacity>
+							<Text style={styles.buttonText}>Next</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
+			</View>
 		</>
 	)
 }
 
 const styles = StyleSheet.create({
-    body: {
-        flex: 0.9
-    },
-    questionContainer: {
-        flex: 0.15,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'dodgerblue'
-    },
-    questionText: {
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    textBoxContainer: {
-        flex: 0.4,
-        backgroundColor: 'seagreen'
-    }
+	body: {
+		flex: 0.9,
+	},
+	questionContainer: {
+		flex: 0.15,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	questionText: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	textBoxContainer: {
+		flex: 0.45,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	textBox: {
+		height: '20%',
+		width: '80%',
+		borderColor: 'gray',
+		backgroundColor: 'lightgrey',
+		borderWidth: 2,
+	},
+	buttonContainer: {
+		flex: 0.3,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	button: {
+		height: 75,
+		width: 200,
+		backgroundColor: 'dodgerblue',
+		justifyContent: 'center',
+		alignItems: 'center',
+		// backgroundColor: 'rgb(31, 116, 255)',
+		backgroundColor: 'dodgerblue',
+	},
+	buttonText: {
+		fontSize: 25,
+		color: 'white',
+	},
 })
 
 export default Memo
